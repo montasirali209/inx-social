43cee50226bc0bf32b6690f3fbc33212ab56c0f7
MD Montasir Ali <120122009+montasirali209@users.noreply.github.com>
2026-09-09 20:05:58 +0100

    Add backend Meta authorization start route

--- inx-social-cloud-backend/src/routes/socialConnectionRoutes.js
+++ inx-social-cloud-backend/src/routes/socialConnectionRoutes.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/socialConnectionController');
 router.get('/oauth/:platform/callback', controller.oauthCallback);
 router.use(requireAuth);
 router.get('/', controller.list);
+router.post('/facebook/start', controller.startFacebook);
 router.post('/oauth/:platform/start', controller.startOAuth);
 router.post('/instagram/sync', controller.syncInstagram);
 router.delete('/:id', controller.disconnect);
