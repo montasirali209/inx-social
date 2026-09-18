2c2213be10b9e43e889bd434a341f62d25aa7504
MD Montasir Ali <120122009+montasirali209@users.noreply.github.com>
2026-09-18 20:04:21 +0100

    Start persistent analytics cache refresh runtime

--- inx-social-cloud-backend/src/server.js
+++ inx-social-cloud-backend/src/server.js
@@ -5,7 +5,7 @@ const { startAgentRuntime } = require('./services/agentRuntimeService');
 const { startMediaRetention } = require('./services/mediaRetentionService');
 const { startStockVideoRuntime } = require('./services/stockVideoStudioService');
 const { startRuntime: startPostForMeRuntime } = require('./services/postForMeService');
-const { startAnalyticsSnapshotRuntime } = require('./services/postForMeAnalyticsService');
+const { startAnalyticsCacheRuntime } = require('./services/postForMeAnalyticsService');
 const prisma = require('./db/prisma');
 
 const server = app.listen(env.port, () => {
@@ -15,7 +15,7 @@ const server = app.listen(env.port, () => {
   startMediaRetention();
   startStockVideoRuntime();
   startPostForMeRuntime();
-  startAnalyticsSnapshotRuntime();
+  startAnalyticsCacheRuntime();
 });
 
 let shuttingDown = false;
